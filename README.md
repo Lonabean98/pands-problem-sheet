af52990fb5b0d6769b667382397b7e494e9e1124
unknown <LonanKeane>
2021-03-04 21:50:08 +0000

    pushing week06 sqrt function to GitHub

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ bmi.py takes an input of hieght in cm and weight in kg and calculates the BMI
 
 weekday.py outputs whether today is a weekday or not.
 I got the code for the day of the week from https://stackoverflow.com/questions/8380389/how-to-get-day-name-from-datetime
+
+
+squareroot.py
+This program takes an input and outputs the square root using Newtons approxiamtion method
+I referenced the code from https://stackoverflow.com/questions/55232484/newtons-method-for-approximating-square-roots in this program
---
+++ squareroot.py
@@ -0,0 +1,32 @@
+#This program takes an input and outputs an approximation of its square root
+#Author: Lonan Keane
+
+#x is you input, converted to a float
+x = float(input("Please enter a positive number: "))  
+
+#defining sqrt function
+def sqrt(x):
+   #The tolerance is the limit of the difference between the input and 
+   #squared Newton approximation
+   tolerance = 0.000001
+   #The estimation is formatted as half of the input value
+   guess = x/2
+   #This statement will loop indefinitely until 
+   # the break condition is met
+   while True:
+        #This changes the value of guess to the result of the Newton
+        #equation
+        guess = (guess + x / guess) / 2
+        #variable to describe the absolute
+        #difference between the input and the Newton Approximation
+        difference = abs(x - guess ** 2)
+        #if the tolerance is greater than the difference between 
+        # Newton approximation and input, it will stop the loop
+        if difference <= tolerance:
+            break
+   #Returns the final Newton approximation
+   return guess
+#Runs the sqrt function using input x 
+sqrt(x)
+#print results of sqrt function rounded to 1 decimal place
+print("The square root of {} is approx" .format(x), round(sqrt(x), 1))
