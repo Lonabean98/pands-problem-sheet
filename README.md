0891e805e17b56c1629581a5d7c34a6c8ae57ce6
unknown <LonanKeane>
2021-03-07 20:53:14 +0000

    pushing week07 task to problem sheet repository

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ weekday.py outputs whether today is a weekday or not.
 I got the code for the day of the week from https://stackoverflow.com/questions/8380389/how-to-get-day-name-from-datetime
 
 
-squareroot.py
-This program takes an input and outputs the square root using Newtons approxiamtion method
+squareroot.py-
+This program takes an input and outputs the square root using Newtons approxiamtion method.
 I referenced the code from https://stackoverflow.com/questions/55232484/newtons-method-for-approximating-square-roots in this program
+
+es.py-
+I found out the method to take an argument from a filename from https://www.tutorialsteacher.com/python/sys-module. I tested that this worked by downloading a different txt file from the internet and inputting their names as an argument, which worked.
---
+++ es.py
@@ -0,0 +1,24 @@
+#This program eads in a text 
+#file and outputs the number of e's it contains.
+#It can also take the filename from an argument on the command line.
+#Author: Lonan Keane
+
+#The sys module provides functions and variables used to 
+# manipulate different parts of the Python runtime environment.
+import sys
+# sys.argv returns a list of command line arguments 
+# passed to a Python script.
+filename= sys.argv[1]
+#opening the argument as f
+with open(filename) as f:
+    #reads all the text from a file into a string. 
+    x=f.read()
+    #This is the start of the e count
+    e_count= 0
+    #looping through every character in the file
+    for letter in x:
+       if letter =='e' or letter== 'E':
+        # if the letter is e or E, add one to the e count
+        e_count += 1
+    #print the final e count
+    print(e_count)
