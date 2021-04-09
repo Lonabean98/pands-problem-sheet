abc4cb1c4b28422b477dcdadd00a4c3e2a32381e
Lonan Keane <LonanKeane>
2021-04-09 21:08:13 +0100

    tidying up the code and comments

--- bmi.py
+++ bmi.py
@@ -1,12 +1,10 @@
 # This program will calculate a persons BMI given height (cm)
 # and weight (kg)
-
 #Author: Lonan Keane
 
 #Inputs for weight and height
-
-weight = int(input("enter weight (kg):")) 
-height = int(input('enter height (cm):'))
+weight = int(input("enter weight (kg): ")) 
+height = int(input('enter height (cm): '))
 
 #conversion of cm to meters squared
 conversion= (height/100)**2
@@ -15,5 +13,4 @@ conversion= (height/100)**2
 BMI = (weight/conversion)
 
 #show BMI results to user, answer is formatted to 2 decimal places
-
-print("BMI:{:.2f}".format(BMI))
+print("BMI is {:.2f}".format(BMI))
